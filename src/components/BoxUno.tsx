26aa8a7c685561aac2dc58c67548f5adaed95ac5
Gustavo <Gustavoblancog29@gmail.com>
2023-01-26 15:14:31 -0400

    box

--- src/components/BoxUno.tsx
+++ src/components/BoxUno.tsx
@@ -7,10 +7,6 @@ export default function () {
         <h1 className={styles.title}> Utilities coin</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque rem, recusandae tenetur placeat corrupti tempore ut ex commodi alias officiis temporibus iste, rerum necessitatibus. Corporis laboriosam incidunt nobis ullam quo.</p>
       </section>
-
-      <section className={styles.section}>
-        
-      </section>
     </div>
   )
 }
